bb9b3314891f2bbcef98d8e52f111770579cbd73
Goktug Erol <ge_dev@yahoo.com>
2022-07-25 04:00:22 -0500

    new_packages-v1

---
+++ app/src/main/java/com/legacycode/kizkiza/activities/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.legacycode.kizkiza.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.legacycode.kizkiza.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
